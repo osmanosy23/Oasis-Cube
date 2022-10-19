ac7633f84b914e6b49816e218316ea73734385ea
Frank Zhou <fcz5@cornell.edu>
2022-10-19 19:06:03 -0400

    reordered the files

--- rubiks/src/cube.ml
+++
@@ -1,2 +0,0 @@
-let cube_rep_r = [| [| "R R R"; "R R R"; "R R R" |] |]
-let () = cube_rep_r |> Array.iter (Array.iter print_endline)
---
+++ src/cube.ml
@@ -0,0 +1,4 @@
+let cube_rep_r =
+  [| [| [| "R"; "R"; "R" |]; [| "R"; "R"; "R" |]; [| "R"; "R"; "R" |] |] |]
+
+let () = cube_rep_r |> Array.iter (Array.iter print_endline)
