34541dce0185ac6679d4f5b39566c3176fc8ab72
Frank Zhou <fcz5@cornell.edu>
2022-10-20 18:44:21 -0400

    added cube.mli

--- src/cube.ml
+++ src/cube.ml
@@ -1,6 +1,2 @@
+open Graphics
 
-
-(* let cube_rep_r =
-  [| [| [| "R"; "R"; "R" |]; [| "R"; "R"; "R" |]; [| "R"; "R"; "R" |] |] |]
-
-let () = cube_rep_r |> Array.iter (Array.iter print_endline) *)
