6acd8255e5307afe62c17ee1291e6bc7dd8f8a67
Asim-Bacchus <asim0531bacchus@gmail.com>
2022-10-20 20:18:11 -0400

    Implement Cube.draw_2dcube
    - Added function to draw one rubik's cube face

--- src/cube.ml
+++ src/cube.ml
@@ -11,15 +11,24 @@ let orange_face = [|Orange;Orange;Orange;Orange;Orange;Orange|]
 let yellow_face = [|Yellow;Yellow;Yellow;Yellow;Yellow;Yellow|]
 let green_face = [|Green;Green;Green;Green;Green;Green|]
 
+let cube = [|white_face; red_face; blue_face; orange_face; yellow_face; green_face|]
 let set_color = function
-| White -> set_color white 
+| White -> set_color magenta 
 | Red -> set_color red 
 | Green -> set_color green 
 | Orange -> set_color (rgb 255 165 0) 
 | Yellow -> set_color yellow 
 | Blue -> set_color blue 
 
+let draw_2dcube cb =
+  for i=0 to 2 do 
+    for j=0 to 2 do 
+      set_color (cb.(j).(i));
+      fill_rect (i * 100) (j * 100) 100 100;
+    done
+  done;;
 
+draw_2dcube cube
    
 
 
