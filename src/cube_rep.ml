62999eb15d318f2b1e7fe2eecc45d6b91e34ccf7
Frank Zhou <fcz5@cornell.edu>
2022-12-02 16:06:03 -0500

    added 2x2 dummy code

--- src/cube_rep.ml
+++ src/cube_rep.ml
@@ -12,9 +12,13 @@ let blue_face = [| Blue; Blue; Blue; Blue; Blue; Blue; Blue; Blue; Blue |]
 let orange_face = [| Orange; Orange; Orange; Orange; Orange; Orange; Orange; Orange; Orange |]
 let yellow_face = [| Yellow; Yellow; Yellow; Yellow; Yellow; Yellow; Yellow; Yellow; Yellow |]
 let green_face = [| Green; Green; Green; Green; Green; Green; Green; Green; Green |]
+let white_face2 = [| White; White; White; White |]
+let red_face2 = [| Red; Red; Red; Red |]
+let blue_face2 = [| Blue; Blue; Blue; Blue |]
+let orange_face2 = [| Orange; Orange; Orange; Orange |]
+let yellow_face2 = [| Yellow; Yellow; Yellow; Yellow |]
+let green_face2 = [| Green; Green; Green; Green |]
 
-(* let cube = [| white_face; red_face; blue_face; orange_face; yellow_face; green_face |] *)
-(* let cube = [| red_face; yellow_face; blue_face; white_face; orange_face; green_face |] *)
 let cube_rep =
   let open Array in
   [| copy orange_face; copy yellow_face; copy green_face; copy white_face; copy red_face; copy blue_face |]
@@ -22,6 +26,14 @@ let cube_rep =
 let solve () =
   let open Array in
   [| copy orange_face; copy yellow_face; copy green_face; copy white_face; copy red_face; copy blue_face |]
+
+let cube_rep2 =
+  let open Array in
+  [| copy orange_face2; copy yellow_face2; copy green_face2; copy white_face2; copy red_face2; copy blue_face2 |]
+
+(* let solve2 () =
+   let open Array in
+   [| copy orange_face2; copy yellow_face2; copy green_face2; copy white_face2; copy red_face2; copy blue_face2 |] *)
 
 let set_cube_color = function
   | White -> set_color white
@@ -51,11 +63,49 @@ let draw_2dcube f x y =
   fill_rect 50 425 900 7;
   fill_rect 50 500 900 7;
   fill_rect 50 575 900 7;
-  
+
   fill_rect 50 350 7 225;
   fill_rect 125 350 7 225;
   fill_rect 200 350 7 225;
-  
+
+  fill_rect 575 350 7 225;
+  fill_rect 650 350 7 225;
+  fill_rect 725 350 7 225;
+  fill_rect 800 350 7 225;
+  fill_rect 875 350 7 225;
+  fill_rect 950 350 7 232;
+
+  fill_rect 275 125 225 7;
+  fill_rect 275 200 225 7;
+  fill_rect 275 275 225 7;
+  fill_rect 275 650 225 7;
+  fill_rect 275 725 225 7;
+  fill_rect 275 800 232 7
+
+let draw_2dcube2 f x y =
+  for j = 0 to 3 do
+    set_cube_color f.(j);
+    match j mod 2 with
+    | 0 -> fill_rect x ((j / 3 * 75) + y) 75 75
+    | 1 -> fill_rect (x + 75) ((j / 3 * 75) + y) 75 75
+    | _ -> failwith "invalid"
+  done;
+  (*the rest of the function draws the black lines separating the colors*)
+  Graphics.set_color black;
+  fill_rect 275 125 7 675;
+  fill_rect 350 125 7 675;
+  fill_rect 425 125 7 675;
+  fill_rect 500 125 7 675;
+
+  fill_rect 50 350 900 7;
+  fill_rect 50 425 900 7;
+  fill_rect 50 500 900 7;
+  fill_rect 50 575 900 7;
+
+  fill_rect 50 350 7 225;
+  fill_rect 125 350 7 225;
+  fill_rect 200 350 7 225;
+
   fill_rect 575 350 7 225;
   fill_rect 650 350 7 225;
   fill_rect 725 350 7 225;
@@ -77,10 +127,18 @@ let draw cube =
   draw_2dcube cube.(2) 500 350;
   draw_2dcube cube.(1) 275 575;
   draw_2dcube cube.(3) 275 125
+
+let draw2 cube =
+  draw_2dcube2 cube.(4) 275 350;
+  draw_2dcube2 cube.(0) 725 350;
+  draw_2dcube2 cube.(5) 50 350;
+  draw_2dcube2 cube.(2) 500 350;
+  draw_2dcube2 cube.(1) 275 575;
+  draw_2dcube2 cube.(3) 275 125
 (* set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1";
    Graphics.draw_string("Counter: 0") *)
 
-let () = draw cube_rep
+let () = draw2 cube_rep2
 
 (*
    Graphics.set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1" *)
