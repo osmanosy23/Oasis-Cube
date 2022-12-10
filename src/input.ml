e2e01de5b9ab7c99458c3ebd36b60c6900006667
Asim-Bacchus <ahb229@cornell.edu>
2022-12-10 02:34:34 -0500

    Fix drawing of 2d 3x3 cube

--- src/input.ml
+++ src/input.ml
@@ -7,6 +7,8 @@ let cube = ref cube_rep
 let cube2 = ref cube_rep2
 let background_color = ref white
 let is_3x3 = ref true
+let is_3d = ref true
+
 let purple = rgb 240 150 240
 let lblue = rgb 157 190 250
 let lred = rgb 255 150 150
@@ -83,16 +85,16 @@ let draw_count count n =
   Graphics.draw_string (count ^ string_of_int !n);
   Graphics.moveto 0 0
 
-let eval_turn turn dim =
-  match dim with
+let eval_turn turn3 turn2  =
+  match !is_3x3 with
   | true ->
-      turn !cube;
-      draw_3d !cube;
+      turn3 !cube;
+      if !is_3d then draw_3d !cube else draw !cube;
       set_color !background_color;
       fill_rect 0 0 1000 100;
       counter := !counter + 1
   | false ->
-      turn !cube2;
+      turn2 !cube2;
       draw2 !cube2;
       set_color !background_color;
       fill_rect 0 0 1000 100;
@@ -101,7 +103,7 @@ let eval_turn turn dim =
 let eval_solve = function
   | true ->
       cube := solve ();
-      draw_3d !cube;
+      if !is_3d then draw_3d !cube else draw !cube;
       set_color !background_color;
       fill_rect 0 0 1000 100;
       counter := 0
@@ -117,7 +119,7 @@ let eval_random n dim =
     match dim with
     | true ->
         randomize !cube n;
-        draw_3d !cube
+        if !is_3d then draw_3d !cube else draw !cube
     | false ->
         randomize2 !cube2 n;
         draw2 !cube2
@@ -126,12 +128,14 @@ let eval_random n dim =
   fill_rect 0 0 1000 100;
   counter := 0
 
-let check_click x y xup xlo yup ylo turn = if xup >= x && x >= xlo && yup >= y && y >= ylo then eval_turn turn !is_3x3
+let check_click x y xup xlo yup ylo turn3 turn2 = 
+  if xup >= x && x >= xlo && yup >= y && y >= ylo then eval_turn turn3 turn2
 
-let change_view (view : cube_type -> unit) dim =
+let change_view (view : cube_type -> unit) is3x3 is3d=
   set_color !background_color;
   fill_rect 0 0 1000000 1000000;
-  match dim with
+  is_3d := is3d;
+  match is3x3 with
   | true ->
       is_3x3 := true;
       eval_solve true;
@@ -157,30 +161,30 @@ let read_key =
           | 'q' -> close_graph ()
           | c -> (
               match c with
-              | 'u' -> eval_turn (if !is_3x3 then u_turn else u_turn2) !is_3x3
-              | 'U' -> eval_turn (if !is_3x3 then u'_turn else u'_turn2) !is_3x3
-              | 'd' -> eval_turn (if !is_3x3 then d_turn else d_turn2) !is_3x3
-              | 'D' -> eval_turn (if !is_3x3 then d'_turn else d'_turn2) !is_3x3
-              | 'b' -> eval_turn (if !is_3x3 then b_turn else b_turn2) !is_3x3
-              | 'B' -> eval_turn (if !is_3x3 then b'_turn else b'_turn2) !is_3x3
-              | 'f' -> eval_turn (if !is_3x3 then f_turn else f_turn2) !is_3x3
-              | 'F' -> eval_turn (if !is_3x3 then f'_turn else f'_turn2) !is_3x3
-              | 'r' -> eval_turn (if !is_3x3 then r_turn else r_turn2) !is_3x3
-              | 'R' -> eval_turn (if !is_3x3 then r'_turn else r'_turn2) !is_3x3
-              | 'l' -> eval_turn (if !is_3x3 then l_turn else l_turn2) !is_3x3
-              | 'L' -> eval_turn (if !is_3x3 then l'_turn else l'_turn2) !is_3x3
-              | 'm' -> eval_turn (if !is_3x3 then m_turn else fun _ -> ()) !is_3x3
-              | 'M' -> eval_turn (if !is_3x3 then m'_turn else fun _ -> ()) !is_3x3
-              | 'e' -> eval_turn (if !is_3x3 then e_turn else fun _ -> ()) !is_3x3
-              | 'E' -> eval_turn (if !is_3x3 then e'_turn else fun _ -> ()) !is_3x3
-              | 's' -> eval_turn (if !is_3x3 then s_turn else fun _ -> ()) !is_3x3
-              | 'S' -> eval_turn (if !is_3x3 then s'_turn else fun _ -> ()) !is_3x3
-              | 'x' -> eval_turn (if !is_3x3 then x_rotate else x_rotate2) !is_3x3
-              | 'X' -> eval_turn (if !is_3x3 then x'_rotate else x'_rotate2) !is_3x3
-              | 'y' -> eval_turn (if !is_3x3 then y_rotate else y_rotate2) !is_3x3
-              | 'Y' -> eval_turn (if !is_3x3 then y'_rotate else y'_rotate2) !is_3x3
-              | 'z' -> eval_turn (if !is_3x3 then z_rotate else z_rotate2) !is_3x3
-              | 'Z' -> eval_turn (if !is_3x3 then z'_rotate else z'_rotate) !is_3x3
+              | 'u' -> eval_turn u_turn u_turn2 
+              | 'U' -> eval_turn u'_turn u'_turn2 
+              | 'd' -> eval_turn d_turn d_turn2
+              | 'D' -> eval_turn d'_turn d'_turn2
+              | 'b' -> eval_turn b_turn b_turn2
+              | 'B' -> eval_turn b'_turn  b'_turn2
+              | 'f' -> eval_turn f_turn  f_turn2
+              | 'F' -> eval_turn f'_turn  f'_turn2
+              | 'r' -> eval_turn r_turn  r_turn2
+              | 'R' -> eval_turn r'_turn  r'_turn2
+              | 'l' -> eval_turn l_turn  l_turn2 
+              | 'L' -> eval_turn l'_turn  l'_turn2 
+              | 'm' -> eval_turn m_turn (fun _ -> ()) 
+              | 'M' -> eval_turn m'_turn (fun _ -> ())
+              | 'e' -> eval_turn e_turn  (fun _ -> ()) 
+              | 'E' -> eval_turn e'_turn  (fun _ -> ()) 
+              | 's' -> eval_turn s_turn  (fun _ -> ()) 
+              | 'S' -> eval_turn s'_turn (fun _ -> ())
+              | 'x' -> eval_turn x_rotate x_rotate2
+              | 'X' -> eval_turn x'_rotate x'_rotate2
+              | 'y' -> eval_turn y_rotate y_rotate2
+              | 'Y' -> eval_turn y'_rotate y'_rotate2 
+              | 'z' -> eval_turn z_rotate z_rotate2
+              | 'Z' -> eval_turn z'_rotate z'_rotate 
               | '.' -> eval_solve !is_3x3
               | '1' -> eval_random 1 !is_3x3
               | '\\' -> eval_random 100 !is_3x3
@@ -196,25 +200,25 @@ let read_key =
                   fill_rect 0 0 1000000 1000000;
                   if !is_3x3 then draw !cube else draw2 !cube;
                   draw_buttons purple
-              | '2' -> change_view draw2 false
-              | '3' -> change_view draw true
-              | '4' -> change_view draw_3d true
+              | '2' -> change_view draw2 false false
+              | '3' -> change_view draw true false
+              | '4' -> change_view draw_3d true true
               | _ -> ())
         else if (not s.button) && !click then
           match (s.mouse_x, s.mouse_y) with
           | a, b ->
-              check_click a b 581 521 297 259 (if !is_3x3 then f_turn else f_turn2);
-              check_click a b 655 595 297 259 (if !is_3x3 then r_turn else r_turn2);
-              check_click a b 729 669 297 259 (if !is_3x3 then u_turn else u_turn2);
-              check_click a b 803 743 297 259 (if !is_3x3 then b_turn else b_turn2);
-              check_click a b 877 817 297 259 (if !is_3x3 then l_turn else l_turn2);
-              check_click a b 951 891 297 259 (if !is_3x3 then d_turn else d_turn2);
-              check_click a b 581 521 245 207 (if !is_3x3 then f'_turn else f'_turn2);
-              check_click a b 655 595 245 207 (if !is_3x3 then r'_turn else r'_turn2);
-              check_click a b 729 669 245 207 (if !is_3x3 then u'_turn else u'_turn2);
-              check_click a b 803 743 245 207 (if !is_3x3 then b'_turn else b'_turn2);
-              check_click a b 877 817 245 207 (if !is_3x3 then l'_turn else l'_turn2);
-              check_click a b 951 891 245 207 (if !is_3x3 then d'_turn else d'_turn2);
+              check_click a b 581 521 297 259 f_turn  f_turn2;
+              check_click a b 655 595 297 259 r_turn r_turn2;
+              check_click a b 729 669 297 259 u_turn u_turn2;
+              check_click a b 803 743 297 259 b_turn b_turn2;
+              check_click a b 877 817 297 259 l_turn l_turn2;
+              check_click a b 951 891 297 259 d_turn d_turn2;
+              check_click a b 581 521 245 207 f'_turn f'_turn2;
+              check_click a b 655 595 245 207 r'_turn r'_turn2;
+              check_click a b 729 669 245 207 u'_turn u'_turn2;
+              check_click a b 803 743 245 207 b'_turn b'_turn2;
+              check_click a b 877 817 245 207 l'_turn l'_turn2;
+              check_click a b 951 891 245 207 d'_turn d'_turn2;
               if 803 >= a && a >= 669 && 141 >= b && b >= 103 then eval_solve !is_3x3;
               if 852 >= a && a >= 620 && 193 >= b && b >= 155 then eval_random 100 !is_3x3;
               click := false
