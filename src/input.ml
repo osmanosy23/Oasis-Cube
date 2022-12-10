a19e2fb55f31f7d646bf18da4a8c3792b4100b19
Frank Zhou <fcz5@cornell.edu>
2022-12-10 08:29:20 -0500

    changed some feature

--- src/input.ml
+++ src/input.ml
@@ -19,18 +19,22 @@ let counter = ref 0
 let ref_c = "Counter: "
 let nextref = ref counter
 let click = ref false
-let darkmode bkgrnd =  
+
+let darkmode bkgrnd =
   set_font "-*-fixed-medium-r-semicondensed--45-*-*-*-*-*-iso8859-1";
-  match bkgrnd with 
-  0xFFFFFF -> fill_rect 108 155 102 38; 
-    moveto 111 151;
-    set_color black; 
-    draw_string "DARK";
-  | 0x323232 -> fill_rect 94 155 130 38;
-    moveto 98 151;
-    set_color black;  
-    draw_string "LIGHT";
+  match bkgrnd with
+  | 0xFFFFFF ->
+      fill_rect 108 155 102 38;
+      moveto 111 151;
+      set_color black;
+      draw_string "DARK"
+  | 0x323232 ->
+      fill_rect 94 155 130 38;
+      moveto 98 151;
+      set_color black;
+      draw_string "LIGHT"
   | _ -> ()
+
 let draw_prime x y =
   set_color black;
   fill_rect x y 3 9
@@ -60,8 +64,8 @@ let draw_buttons color =
   set_color lyellow;
   fill_rect 92 207 60 38;
   fill_rect 166 207 60 38;
-  set_color gray; 
-  darkmode !background_color; 
+  set_color gray;
+  darkmode !background_color;
   set_color black;
   set_font "-*-fixed-medium-r-semicondensed--45-*-*-*-*-*-iso8859-1";
   moveto 543 255;
@@ -121,27 +125,27 @@ let eval_turn turn3 turn2 valid =
       if !is_3d then draw_3d !cube else draw !cube;
       set_color !background_color;
       fill_rect 0 0 1000 100;
-      counter := !counter + 1
+      incr counter
   | false ->
       turn2 !cube2;
       if !is_3d then draw2_3d !cube2 else draw2 !cube2;
       set_color !background_color;
       fill_rect 0 0 1000 100;
-      if valid then counter := !counter + 1 
+      if valid then incr counter
 
-let eval_solve = function
-  | true ->
-      cube := solve ();
-      if !is_3d then draw_3d !cube else draw !cube;
-      set_color !background_color;
-      fill_rect 0 0 1000 100;
-      counter := 0
-  | false ->
-      cube2 := solve2 ();
-      if !is_3d then draw2_3d !cube2 else draw2 !cube2;
-      set_color !background_color;
-      fill_rect 0 0 1000 100;
-      counter := 0
+let eval_solve dim =
+  let _ =
+    match dim with
+    | true ->
+        cube := solve ();
+        if !is_3d then draw_3d !cube else draw !cube
+    | false ->
+        cube2 := solve2 ();
+        if !is_3d then draw2_3d !cube2 else draw2 !cube2
+  in
+  set_color !background_color;
+  fill_rect 0 0 1000 100;
+  counter := 0
 
 let eval_random n dim =
   let _ =
@@ -157,54 +161,50 @@ let eval_random n dim =
   fill_rect 0 0 1000 100;
   counter := 0
 
-let check_click x y xup xlo yup ylo turn3 turn2 = 
+let check_click x y xup xlo yup ylo turn3 turn2 =
   if xup >= x && x >= xlo && yup >= y && y >= ylo then eval_turn turn3 turn2 true
 
-let change_view is3x3 is3d=
+let change_view is3x3 is3d =
   is_3x3 := is3x3;
   is_3d := is3d;
   set_color !background_color;
   fill_rect 0 0 1000000 1000000;
   is_3d := is3d;
-  match is3x3, is3d with
+  match (is3x3, is3d) with
   | true, true ->
-      eval_solve true;
       draw_3d !cube;
       draw_buttons purple
   | false, true ->
-      eval_solve false;
       draw2_3d !cube2;
       draw_buttons purple
   | true, false ->
-      eval_solve true;
       draw !cube;
       draw_buttons purple
   | false, false ->
-      eval_solve false;
       draw2 !cube2;
       draw_buttons purple
-  
-  let change_background color = 
-    background_color := color;
-    set_color !background_color;
-    fill_rect 0 0 1000000 1000000;
-    match !is_3x3, !is_3d with
-    | true, true ->
+
+let change_background color =
+  background_color := color;
+  set_color !background_color;
+  fill_rect 0 0 1000000 1000000;
+  match (!is_3x3, !is_3d) with
+  | true, true ->
       draw_3d !cube;
       draw_buttons purple
-    | false, true ->
+  | false, true ->
       draw2_3d !cube2;
       draw_buttons purple
-    | true, false ->
+  | true, false ->
       draw !cube;
       draw_buttons purple
-    | false, false ->
+  | false, false ->
       draw2 !cube2;
       draw_buttons purple
 
 let check_dark_click x y bkgrnd =
-  match bkgrnd with 
-  0xFFFFFF -> if 210 >= x && x >= 108 && 500 >= y && y >= 155 then change_background 0x323232
+  match bkgrnd with
+  | 0xFFFFFF -> if 210 >= x && x >= 108 && 500 >= y && y >= 155 then change_background 0x323232
   | 0x323232 -> if 224 >= x && x >= 94 && 193 >= y && y >= 155 then change_background white
   | _ -> ()
 
@@ -226,39 +226,39 @@ let read_key =
               | 'd' -> eval_turn d_turn d_turn2 true
               | 'D' -> eval_turn d'_turn d'_turn2 true
               | 'b' -> eval_turn b_turn b_turn2 true
-              | 'B' -> eval_turn b'_turn  b'_turn2 true
-              | 'f' -> eval_turn f_turn  f_turn2 true
-              | 'F' -> eval_turn f'_turn  f'_turn2 true
-              | 'r' -> eval_turn r_turn  r_turn2 true
-              | 'R' -> eval_turn r'_turn  r'_turn2 true
-              | 'l' -> eval_turn l_turn  l_turn2 true 
-              | 'L' -> eval_turn l'_turn  l'_turn2 true 
-              | 'm' -> eval_turn  m_turn (fun _ -> ()) false 
+              | 'B' -> eval_turn b'_turn b'_turn2 true
+              | 'f' -> eval_turn f_turn f_turn2 true
+              | 'F' -> eval_turn f'_turn f'_turn2 true
+              | 'r' -> eval_turn r_turn r_turn2 true
+              | 'R' -> eval_turn r'_turn r'_turn2 true
+              | 'l' -> eval_turn l_turn l_turn2 true
+              | 'L' -> eval_turn l'_turn l'_turn2 true
+              | 'm' -> eval_turn m_turn (fun _ -> ()) false
               | 'M' -> eval_turn m'_turn (fun _ -> ()) false
-              | 'e' -> eval_turn e_turn  (fun _ -> ()) false 
-              | 'E' -> eval_turn e'_turn  (fun _ -> ()) false 
-              | 's' -> eval_turn s_turn  (fun _ -> ()) false 
+              | 'e' -> eval_turn e_turn (fun _ -> ()) false
+              | 'E' -> eval_turn e'_turn (fun _ -> ()) false
+              | 's' -> eval_turn s_turn (fun _ -> ()) false
               | 'S' -> eval_turn s'_turn (fun _ -> ()) false
               | 'x' -> eval_turn x_rotate x_rotate2 true
               | 'X' -> eval_turn x'_rotate x'_rotate2 true
               | 'y' -> eval_turn y_rotate y_rotate2 true
-              | 'Y' -> eval_turn y'_rotate y'_rotate2 true 
+              | 'Y' -> eval_turn y'_rotate y'_rotate2 true
               | 'z' -> eval_turn z_rotate z_rotate2 true
-              | 'Z' -> eval_turn z'_rotate z'_rotate true 
+              | 'Z' -> eval_turn z'_rotate z'_rotate true
               | '.' -> eval_solve !is_3x3
               | '1' -> eval_random 1 !is_3x3
               | '\\' -> eval_random 100 !is_3x3
               | ',' -> change_background 0x323232
               | '<' -> change_background white
-              | '2' -> change_view  false false
-              | '3' -> change_view  true false
-              | '4' -> change_view  false true
-              | '5' -> change_view  true true
+              | '2' -> change_view false false
+              | '3' -> change_view true false
+              | '4' -> change_view false true
+              | '5' -> change_view true true
               | _ -> ())
         else if (not s.button) && !click then
           match (s.mouse_x, s.mouse_y) with
           | x, y ->
-              check_click x y 581 521 297 259 f_turn  f_turn2;
+              check_click x y 581 521 297 259 f_turn f_turn2;
               check_click x y 655 595 297 259 r_turn r_turn2;
               check_click x y 729 669 297 259 u_turn u_turn2;
               check_click x y 803 743 297 259 b_turn b_turn2;
