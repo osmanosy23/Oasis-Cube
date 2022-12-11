8e194ab9c56c4e890edae1fde1ae2835c21cfff4
Frank Zhou <fzcuber@gmail.com>
2022-12-11 10:54:34 -0500

    fixed make opendoc

--- opendoc.sh
+++ opendoc.sh
@@ -2,11 +2,11 @@
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
-  open _build/default/_doc/_html/cube/
+  open _build/default/_doc/_html/index.html/
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   if [[ -n "$IS_WSL" || -n "$WSL_DISTRO_NAME" ]]; then
     # WSL
-    DOCPATH=$(wslpath -w ./_build/default/_doc/_html/cube/)
+    DOCPATH=$(wslpath -w ./_build/default/_doc/_html/index.html)
     explorer.exe ${DOCPATH} || true
   else
     nautilus _build/default/_doc/_html/cube/
